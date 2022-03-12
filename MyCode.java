fc37c194f79dc3a06bd9ae5f0e8a739d4fb7af89
user01 <user01@localhost.localdomain>
2022-03-11 16:21:09 -0800

    2nd master commit

--- MyCode.java
+++ MyCode.java
@@ -1,5 +1,6 @@
 class MyCode{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello World");
+     System.out.println("Happy days");  
     }  
 }  

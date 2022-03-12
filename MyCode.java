104f2a90a6e7268eb83f8b74746c609f9e4a232b
user01 <user01@localhost.localdomain>
2022-03-11 16:24:19 -0800

    Branch commit

--- MyCode.java
+++ MyCode.java
@@ -1,5 +1,6 @@
 class MyCode{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java");
+     System.out.println("Let there be peace");  
     }  
 }  
